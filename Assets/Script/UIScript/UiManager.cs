65bb0aff43f32df0483e86425ec70a76a3ba2ad6
김도현 <word9574@naver.com>
2018-04-12 18:08:53 +0900

    Add ArtResource and Setting popup

---
+++ Assets/Script/UIScript/UiManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour {
+
+	public GameObject Setting;
+
+	public void OnSetting(){
+		Setting.SetActive (true);
+	}
+
+	public void OffSetting(){
+		Setting.SetActive (false);
+	}
+	
+}
