a65a7fa4bde1dfd3c09adb548ae29e3d234d5a9b
김도현 <word9574@naver.com>
2018-04-18 18:15:32 +0900

    Add Editor

--- Assets/Editor/ScriptEditor/FriendShipConditionEditor.cs
+++ Assets/Editor/ScriptEditor/FriendShipConditionEditor.cs
@@ -1,16 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+#if UNITY_EDITOR 
+using UnityEditor;
+#endif 
 
-public class FriendShipConditionEditor : MonoBehaviour {
+[CustomEditor(typeof(FriendShipCondition))]
+public class FriendShipConditionEditor : EditorWithSubEditors<ConditionEditor, Condition> {
 
-	// Use this for initialization
-	void Start () {
-		
+	public SerializeField condition;
+
+	private FriendShipCondition friendShipCondition;
+
+	private SerializedProperty descriptionProperty;
+	private SerializedProperty requireFriendlyProperty;
+	private SerializedProperty addConditionProperty;
+
+	private const string descriptionPropertyName = "description";
+	private const string requireFriendlyPropertyName = "requireFriendly";
+	private const string addConditionPropertyName = "addCondition";
+
+	private void OnEnable()
+	{
+		friendShipCondition = (FriendShipCondition)target;
+
+		if (target == null)
+		{
+			DestroyImmediate (this);
+			return;
+		}
+
+		descriptionProperty = serializedObject.FindProperty (descriptionPropertyName);
+		requireFriendlyProperty = serializedObject.FindProperty (requireFriendlyPropertyName);
+		addConditionProperty = serializedObject.FindProperty (addConditionPropertyName);
+
+		CheckAndCreateSubEditors (friendShipCondition.addCondition);
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void OnDisable ()
+	{
+		CleanupEditors ();
+	}
+
+	protected override void SubEditorSetup (ConditionEditor editor)
+	{
+		editor.editorType = ConditionEditor.EditorType.ConditionCollection;
+		editor.conditionsProperty = addConditionProperty;
+	}
+
+	public override void OnInspectorGUI(){
+		EditorGUILayout.Space();
+
+		EditorGUILayout.PropertyField(descriptionProperty);
+
+		EditorGUILayout.Space();
+
+		float space = EditorGUIUtility.currentViewWidth / 3f;
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Condition", GUILayout.Width(space));
+		EditorGUILayout.LabelField("Satisfied?", GUILayout.Width(space));
+		EditorGUILayout.LabelField("Add/Remove", GUILayout.Width(space));
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+		for (int i = 0; i < subEditors.Length; i++)
+		{
+			subEditors[i].OnInspectorGUI();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace ();
+		if (GUILayout.Button("+", GUILayout.Width(125f)))
+		{
+			Condition newCondition = ConditionEditor.CreateCondition();
+			addConditionProperty.AddToObjectArray(newCondition);
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+
+	private void ExpandedGUI(){
+
 	}
 }
--- Assets/Editor/ScriptEditor/FriendShipEditor.cs
+++ Assets/Editor/ScriptEditor/FriendShipEditor.cs
@@ -1,16 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+#if UNITY_EDITOR 
+using UnityEditor;
+#endif 
 
-public class FriendShipEditor : MonoBehaviour {
+[CustomEditor(typeof(FriendShip))]
+public class FriendShipEditor : EditorWithSubEditors<FriendShipConditionEditor, FriendShipCondition> {
 
-	// Use this for initialization
-	void Start () {
-		
+	private FriendShip friendShip;
+	private SerializedProperty friendlyProperty;
+	private SerializedProperty friendShipConditionProperty;
+
+	private const float collectionButtonWidth = 125f;
+	private const string friendlyPropertyName = "friendly";
+	private const string friendShipConditionPropertyName = "friendShipCondition";
+
+	private void OnEnable ()
+	{
+		friendShip = (FriendShip)target;
+
+		friendlyProperty = serializedObject.FindProperty(friendlyPropertyName);
+		friendShipConditionProperty = serializedObject.FindProperty(friendShipConditionPropertyName);
+
+
+		CheckAndCreateSubEditors(friendShip.friendShipCondition);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	private void OnDisable ()
+	{
+		CleanupEditors ();
+	}
+
+
+	protected override void SubEditorSetup(FriendShipConditionEditor editor)
+	{
+		//editor.condition = friendShipConditionProperty;
+	}
+
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update ();
+
+		CheckAndCreateSubEditors(friendShip.friendShipCondition);
+
+		for (int i = 0; i < subEditors.Length; i++)
+		{
+			//subEditors[i].OnInspectorGUI ();
+			EditorGUILayout.Space ();
+		}
+
+		EditorGUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace ();
+		if (GUILayout.Button("Add Collection", GUILayout.Width(collectionButtonWidth)))
+		{
+			ConditionCollection newCollection = ConditionCollectionEditor.CreateConditionCollection ();
+			friendShipConditionProperty.AddToObjectArray (newCollection);
+		}
+		EditorGUILayout.EndHorizontal ();
+
+		EditorGUILayout.Space ();;
+
+		serializedObject.ApplyModifiedProperties ();
 	}
 }
