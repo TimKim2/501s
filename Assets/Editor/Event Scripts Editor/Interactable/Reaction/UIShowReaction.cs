29d612b9a7e0e572a90b2d00f7e24c2c7adaf9ab
kangms5296 <rlfhfhdhk@naver.com>
2018-04-20 10:27:18 +0900

    일단 문제가 되었던 InventoryItem Script 삭제. Inventory에 물건을 넣는 과정은 모여서 구조 정리하고 해야할 듯.

---
+++ Assets/Editor/Event Scripts Editor/Interactable/Reaction/UIShowReaction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UIShowReaction : DelayedReaction
+{
+
+    public GameObject[] UI;
+
+    protected override void ImmediateReaction()
+    {
+        foreach (var ui in UI)
+            ui.gameObject.SetActive(true);
+    }
+}
