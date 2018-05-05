15aa9bfd645ea24b79362971fe70f7a2d158c1dd
김도현 <word9574@naver.com>
2018-05-05 15:33:43 +0900

    Add Art Resource

--- Assets/Resources/EventMaster.cs
+++ Assets/Resources/EventMaster.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventMaster : MonoBehaviour {
+public enum SCENE_NAME{ 
+	SCENE_01, SCENE_02, SELECT, 
+}
+
+[CreateAssetMenu]
+public class EventMaster : ScriptableObject {
 
 	private static EventMaster m_instance;
 	private const string loadPath = "EventMaster";
 
 	public ReactionCollection currentReactionCollection;
+
+	public SCENE_NAME cutSceneName;
 
 	public static EventMaster Instance{
 		get{
