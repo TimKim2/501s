c44319f2e7aaa83ffbac482c3ea6206dff5a80a8
김도현 <word9574@naver.com>
2018-05-12 00:09:25 +0900

    Revert "Add Reaction"
    This reverts commit f6ae6f136d712fd525b5518c6bc729b1f9a3605e.

---
+++ Assets/Script/NPC_Script/NpcOnClick.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NpcOnClick : MonoBehaviour{
+
+	public Interactable npcInteractable;
+
+	private MeshRenderer mesh;
+
+	private bool isClick = false;
+
+
+	// Use this for initialization
+	void Start () {
+		mesh = gameObject.GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void OnMouseDown()
+	{
+		mesh.material.color = Color.red;
+		isClick = true;
+
+	}
+
+	public void OnMouseUp()
+	{
+		Debug.Log ("OnMouseDown");
+		if (isClick) {
+			if (npcInteractable != null) {
+
+				Debug.Log ("Event Start");
+				mesh.material.color = Color.green;
+
+				npcInteractable.Interact ();
+		
+			}
+		}
+	}
+
+	public void OnMouseExit()
+	{
+		isClick = false;
+		mesh.material.color = Color.green;
+	}
+
+	public void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.transform.tag == "Player") {
+			Debug.Log ("Col");
+		}
+	}
+}
